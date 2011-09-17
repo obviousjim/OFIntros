09ce6a21648d2878099d0f716b22c256763a1d30
supervisor <supervisor@razor.(none)>
2011-09-17 14:06:22 +0100

    Added linux code::blocks project files

--- 03_AppToAppReceiveComplete/src/testApp.h
+++ 03_AppToAppReceiveComplete/src/testApp.h
@@ -4,29 +4,30 @@
 #include "ofxOsc.h"
 
 /***************************************************************
- 
+
  AppToAppReceivingExample
  - this app demonstrates using the ofxOsc addon
  - use with AppToAppSending example to send keyboard
  commands from application to application
- 
+
  SETUP:
  - One computer:
 	- compile and run!
  - Two computers:
-	- see the setup step in the AppToAppSending example
- 
+	- see the setup step in the AppToAppS
+	ending example
+
  ***************************************************************/
 
 class testApp : public ofBaseApp{
 	public:
 
 		// default OF methods
-		
+
 		void setup();
 		void update();
 		void draw();
-		
+
 		void keyPressed(int key);
 		void keyReleased(int key);
 		void mouseMoved(int x, int y );
@@ -35,13 +36,13 @@ class testApp : public ofBaseApp{
 		void mouseReleased(int x, int y, int button);
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);		
+		void gotMessage(ofMessage msg);
 
 		// AppToAppReceiving methods
-	
+
 		ofTrueTypeFont		font;		// font to draw incoming messages
 		ofxOscReceiver		receiver;	// OSC receiver
 		int					port;		// port we're listening on: must match port from sender!
-	
+
 		string				typing;		//typing you've received from another app
 };
