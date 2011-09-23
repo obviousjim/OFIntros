f83df5d7bebd93423e45bbe62ddbeeaa0f6c35c2
bilderbuchi <bilderbuchi@phononoia.at>
2011-09-23 18:52:14 +0200

    Add framerate limiter to sender

--- 03_AppToAppSendComplete/src/testApp.cpp
+++ 03_AppToAppSendComplete/src/testApp.cpp
@@ -3,32 +3,35 @@
 //--------------------------------------------------------------
 void testApp::setup(){
 
+    ofSetFrameRate(60); // run at 60 fps
+	ofSetVerticalSync(true);
+
 	// set the background color to a lovely gray
 	ofBackground( 150 );
 
 	/*
 		SETUP OSC SENDER
-		
+
 		usage: sender.setup( string host, int port );
-			host: 
+			host:
 				- if on one computer, set to "localhost"
 				- if sending to another computer, set to the IP address
 				  of second computer (found in network settings), e.g. "192.168.1.1"
 			port:
 				- this is arbitrary, but MUST match the port of the reciever application
 	*/
-	
+
 	host	= "localhost";
 	port	= 9000;
-	
+
 	sender.setup( host, port );
-	
+
 	/*
 		Load font to display instructions + what you've typed
 		simplest usage:
 			font.loadFont(string filename, int fontsize );
 	*/
-	
+
 	debugfont.loadFont("futura_book.otf", 12);
 	giantFont.loadFont("futura_book.otf", 20);
 }
@@ -40,26 +43,26 @@ void testApp::update(){
 
 //--------------------------------------------------------------
 void testApp::draw(){
-	
+
 	// draw some debug data + instructions to the screen with the font you loaded
-	
+
 	string debug			= "sending osc messages to " + string( host ) +" on port "+ ofToString( port );
 	string instructions		= "type to create a new message. hit RETURN to send!";
-	
+
 	debugfont.drawString( debug, 20, 20);
 	debugfont.drawString( instructions, 20, 40);
-	
+
 	// what have we typed so far?
-	
+
 	giantFont.drawString( typing, 20, 80);
 }
 
 //--------------------------------------------------------------
 void testApp::keyPressed  (int key){
-	
+
 	// if we didn't hit return, add the key to our string
 	if ( key != OF_KEY_RETURN ){
-		
+
 		// some trickery: ignore the backspace key
 		if (key != OF_KEY_BACKSPACE){
 			typing += key;
@@ -68,19 +71,19 @@ void testApp::keyPressed  (int key){
 				typing.erase( typing.end() - 1 );
 			}
 		};
-	} 
-	
+	}
+
 	// hit Return, time to send the osc message
 	else {
-		
+
 		// to send a string, create an ofxOscMessage object, give it an address
 		// and add a string argument to the object
-		
+
 		ofxOscMessage m;
 		m.setAddress( "/typing" );
 		m.addStringArg( typing );
 		sender.sendMessage( m );
-		
+
 		// clear out "typing"
 		typing = "";
 	}
